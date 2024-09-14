0b195bfa9f9fedb52be0394e54bf9f916182a821
qreilly00 <qreilly00@gmail.com>
2024-09-14 22:47:14 +0100

    fix glaring issue in logic.

--- index.js
+++ index.js
@@ -17,16 +17,21 @@ async function getAttributes() {
   // Grab the text from the title attribute that holds the values.
   let results = [];
 
-  while (results.length <= 100) {
-    results = results.concat(await page.getByTitle(title_date_time_regex).all());
+  //results = results.concat(await page.getByTitle(title_date_time_regex).all());
+
+  // Encountered interesting issue here. Originally the extract titles function wasn't called until all attributes were collected. The problem is, when the extract titles func was run, it only had access to the most recent page after the more button was pressed. This caused all instances of titles from ther previous 3-4 pages to be taken solely from the last page. Fixed by taking titles before the next page was loaded.
+  do {
+    results = results.concat(
+      await extractTitles(await page.getByTitle(title_date_time_regex).all())
+    );
     await clickMore(page);
-  }
+  } while (results.length <= 100);
 
   return results;
 }
 
 async function clickMore(page) {
-  page.getByRole('more').click();
+  await page.getByRole('link', { name: 'More', exact: true }).click();
 }
 
 async function extractTitles(results) {
@@ -54,7 +59,7 @@ function parseDateAndTime(results_titles) {
     const time = time_only_regex.exec(results_titles[i]);
 
     if (date != null && time != null) {
-      date_and_time = date[0] + " " + time[0];
+      date_and_time = new Date(date[0] + "T" + time[0]);
       date_and_time_parsed.push(date_and_time);
     }
     else {
@@ -62,26 +67,26 @@ function parseDateAndTime(results_titles) {
     }
   }
 
-  testPrintArray(date_and_time_parsed, 100);
-  console.log(read_errors, "read errors detected");
+  testPrintDateTimeArray(date_and_time_parsed, date_and_time_parsed.length);
+  console.log(read_errors, "read errors detected.");
 
   return date_and_time_parsed;
 }
 
 function testPrintArray(array) {
-  for (let i = 0;  i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     console.log(array[i]);
   }
 }
 
-function testPrintArray(array, count) {
-  for (let i = 0;  i < count; i++) {
-    console.log(array[i]);
+function testPrintDateTimeArray(array, count) {
+  for (let i = 0; i < count; i++) {
+    console.log(array[i].toLocaleString());
   }
 }
 
 function testPrint2dArray(array) {
-  for (let i = 0;  i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     console.log(array[i][0], array[i][1]);
   }
 }
@@ -89,15 +94,15 @@ function testPrint2dArray(array) {
 async function sortHackerNewsArticles() {
   // Grab at least 100 articles.
   const results = await getAttributes();
-  const results_titles = await extractTitles(results);
-  const date_and_time_parsed = parseDateAndTime(results_titles);
+  //const results_titles = await extractTitles(results); // This is where the issue was originally.
+  const date_and_time_parsed = parseDateAndTime(results);
 
   // check for all records for an out of order date/time.
   let errors = 0;
   let checks = 0;
 
-  for (let i = 1; i <= 100; i++) {
-    if (!date_and_time_parsed[i - 1] > date_and_time_parsed[i]) {
+  for (let i = 1; i < date_and_time_parsed.length; i++) {
+    if (!(date_and_time_parsed[i - 1] >= date_and_time_parsed[i])) {
       errors++;
     }
 
